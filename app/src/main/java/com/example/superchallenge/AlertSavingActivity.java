a5b5a55c80afd8abb7a42394be874eccfc2355b0
Eunno An <12171647@inha.edu>
2020-01-08 20:08:57 +0900

    LoginActivity -> MainActivity Intent success Second Test Commit
    -------------------------
    2020-01-08 pm 08:08
    qr code create & scan success

--- app/src/main/java/com/example/superchallenge/AlertSavingActivity.java
+++ app/src/main/java/com/example/superchallenge/AlertSavingActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class QRCameraActivity extends AppCompatActivity {
+public class AlertSavingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_qrcamera);
+        setContentView(R.layout.activity_alert_saving);
     }
 }
--- app/src/main/java/com/example/superchallenge/MainActivity.java
+++ app/src/main/java/com/example/superchallenge/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.superchallenge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 //2010-01-08 MainActivity Design Start
@@ -10,10 +13,28 @@ public class MainActivity extends AppCompatActivity {
 
     //AZZZZZZZZZZZ
 
+    private Button createQRBtn;
+    private Button scanQRBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createQRBtn = (Button) findViewById(R.id.createQR);
+        scanQRBtn = (Button) findViewById(R.id.scanQR);
+
+        createQRBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, QRCreateActivity.class);
+                startActivity(intent);
+            }
+        });
+        scanQRBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, QRScanActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
