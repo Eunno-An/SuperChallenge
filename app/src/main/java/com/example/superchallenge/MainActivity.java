733fe4171ad0d769b5e58d1a8bd55104c40af999
Eunno An <12171647@inha.edu>
2020-01-08 16:52:00 +0900

    LoginActivity -> MainActivity Intent success

--- app/src/main/java/com/example/superchallenge/MainActivity.java
+++ app/src/main/java/com/example/superchallenge/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//2010-01-08 MainActivity Design Start
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
