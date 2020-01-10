1035e65bd97032e8dca88215494be4b41d1dcac4
Eunno An <12171647@inha.edu>
2020-01-10 23:44:40 +0900

    LoginActivity -> MainActivity Intent success Second Test Commit
    -------------------------
    2020-01-08 pm 08:08
    qr code create & scan success
    
    -------------------------
    2020-01-08 pm 09:34
    main.xml design
    -------------------------
    2020-01-08 pm 10:48
    QR scan -> if success -> AlertSavingActivity
            -> else -> LoginActivity
    
    -------------------------
    2020-01-09 pm 01:47
    navigation drawer success
    
    -------------------------
    2020-01-09 pm 03:36
    intent add
    
    -------------------------
    2020-01-09 pm 04:37
    google map processing
    
    -------------------------
    2020-01-10 pm 07:43
    add blank
    
    -------------------------
    2020-01-10 pm 09:01
    intent activity success
    
    ------------------------
    2020-01-10
    activity_main.xml design success
    
    --------------------------
    2020-01-10
    navigation drawer menu.xml
    icon 수정
    
    ------------------------
    2020-01-10 pm 11:44
    google map icon fix

--- app/src/main/java/com/example/superchallenge/FindMapActivity.java
+++ app/src/main/java/com/example/superchallenge/FindMapActivity.java
@@ -1,13 +1,24 @@
 package com.example.superchallenge;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Region;
+import android.graphics.drawable.BitmapDrawable;
+import android.location.Location;
 import android.os.Bundle;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -32,13 +43,47 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 
 public class FindMapActivity extends AppCompatActivity implements OnMapReadyCallback,NavigationView.OnNavigationItemSelectedListener {
-    private GoogleMap mMap;
+    private GoogleMap mGoogleMap;
+    private Region region;
+    private boolean isMarkChecked = false;
+    private GoogleApiClient mGoogleApiClient = null;
+    private Marker currentMarker = null;
+    private int cameraCount = 0;
+    private LatLng destinationLatLng;
+    private MarkerOptions markerOptions;
+    private Marker m;
+    private Bitmap smallMarker;
+    private CircleOptions circle5M;
+    private Circle circleObject;
+    private static final String TAG = "googlemap_example";
+    private static final int GPS_ENABLE_REQUEST_CODE = 2001;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 2002;
+    private static final int UPDATE_INTERVAL_MS = 500;  // 1초
+    private static final int FASTEST_UPDATE_INTERVAL_MS = 250; // 0.25
+
+    private AppCompatActivity mActivity;
+    boolean askPermissionOnceAgain = false;
+    boolean mRequestingLocationUpdates = false;
+    Location mCurrentLocatiion;
+    boolean mMoveMapByUser = true;
+    boolean mMoveMapByAPI = true;
+    LatLng currentPosition;
+    LocationRequest locationRequest = new LocationRequest()
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+            .setInterval(UPDATE_INTERVAL_MS)
+            .setFastestInterval(FASTEST_UPDATE_INTERVAL_MS);
+
     private AppBarConfiguration mAppBarConfiguration;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_map);
+
+
 
 
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -71,25 +116,80 @@ public class FindMapActivity extends AppCompatActivity implements OnMapReadyCall
         NavigationUI.setupWithNavController(navigationView, navController);
         navigationView.setNavigationItemSelectedListener(this);
 
+        //SupportMapFragment을 통해 레이아웃에 만든 fragment의 ID를 참조하고 구글맵을 호출한다.
         SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        navigationView.setNavigationItemSelectedListener(this);
+
+
+
+
     }
-
-
-
     @Override
     public void onMapReady(final GoogleMap googleMap){
-        mMap = googleMap;
-        LatLng SEOUL = new LatLng(37.56, 126.97);
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(SEOUL);
-        markerOptions.title("서울");
-        markerOptions.snippet("한국의 수도");
-        mMap.addMarker(markerOptions);
+        mGoogleMap = googleMap;
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        //지도의 초기위치를 서울로 이동
+        setDefaultLocation();
+        //인하대학교 하이테크 센터로 위치 설정
+        LatLng INCHEON_INHAUNIV_HIGHTECH = new LatLng(37.450686, 126.657126);
+        LatLng INCHEON_INHAUNIV_5ARCH = new LatLng(37.451351, 126.653924);
+        LatLng INCHEON_INHAUNIV_JEONGSEK = new LatLng(37.449426, 126.652536);
+        //구글 맵 마커에 대한 옵션 설정
+        MarkerOptions markerOptions0 = new MarkerOptions();
+        markerOptions0.position(INCHEON_INHAUNIV_HIGHTECH);
+        markerOptions0.title("인하대학교 하이테크센터");
+
+        MarkerOptions markerOptions1 = new MarkerOptions();
+        markerOptions1.position(INCHEON_INHAUNIV_5ARCH);
+        markerOptions1.title("인하대학교 5호관 입구");
+
+        MarkerOptions markerOptions2 = new MarkerOptions();
+        markerOptions2.position(INCHEON_INHAUNIV_JEONGSEK);
+        markerOptions2.title("인하대학교 정석학술정보관");
+
+        //마커 이미지를 변경한다.
+        BitmapDrawable bitmapdraw=(BitmapDrawable)getResources().getDrawable(R.drawable.icon_main);
+        Bitmap b=bitmapdraw.getBitmap();
+        Bitmap smallMarker = Bitmap.createScaledBitmap(b, 100, 100, false);
+
+        markerOptions0.icon(BitmapDescriptorFactory.fromBitmap(smallMarker));
+        markerOptions1.icon(BitmapDescriptorFactory.fromBitmap(smallMarker));
+        markerOptions2.icon(BitmapDescriptorFactory.fromBitmap(smallMarker));
+
+        //마커를 생성한다.
+        mGoogleMap.addMarker(markerOptions0);
+        mGoogleMap.addMarker(markerOptions1);
+        mGoogleMap.addMarker(markerOptions2);
+
+
+        //카메라를 인하대학교 위치로 옮긴다.
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(INCHEON_INHAUNIV_HIGHTECH));
+        mGoogleMap.animateCamera(CameraUpdateFactory.zoomTo(17));
+
+
+
+    }
+    //default location을 설정
+    public void setDefaultLocation(){
+        mMoveMapByUser = false;
+        //디폴트 위치, Seoul
+        LatLng DEFAULT_LOCATION = new LatLng(37.56, 126.97);
+        String markerTitle = "위치정보 가져올 수 없음";
+        String markerSnippet = "위치 퍼미션과 GPS 활성 요부 확인하세요";
+        if (currentMarker != null) currentMarker.remove();
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(DEFAULT_LOCATION);
+        markerOptions.title(markerTitle);
+        markerOptions.snippet(markerSnippet);
+        markerOptions.draggable(true);
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+        currentMarker = mGoogleMap.addMarker(markerOptions);
+
+
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(DEFAULT_LOCATION, 15);
+        mGoogleMap.moveCamera(cameraUpdate);
     }
 
     @Override
@@ -101,7 +201,6 @@ public class FindMapActivity extends AppCompatActivity implements OnMapReadyCall
             super.onBackPressed();
         }
     }
-
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
@@ -133,7 +232,6 @@ public class FindMapActivity extends AppCompatActivity implements OnMapReadyCall
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -154,7 +252,6 @@ public class FindMapActivity extends AppCompatActivity implements OnMapReadyCall
         getMenuInflater().inflate(R.menu.find_map, menu);
         return true;
     }
-
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
