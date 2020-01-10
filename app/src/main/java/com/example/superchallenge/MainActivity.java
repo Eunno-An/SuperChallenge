28090a76dc9964bca2c4d27508a802b201b5403c
Eunno An <12171647@inha.edu>
2020-01-10 19:44:06 +0900

    LoginActivity -> MainActivity Intent success Second Test Commit
    -------------------------
    2020-01-08 pm 08:08
    qr code create & scan success
    
    -------------------------
    2020-01-08 pm 09:34
    main.xml design
    -------------------------
    2020-01-08 pm 10:48
    QR scan -> if success -> AlertSavingActivity
            -> else -> LoginActivity
    
    -------------------------
    2020-01-09 pm 01:47
    navigation drawer success
    
    -------------------------
    2020-01-09 pm 03:36
    intent add
    
    -------------------------
    2020-01-09 pm 04:37
    google map processing
    
    -------------------------
    2020-01-10 pm 07;43
    add blank

--- app/src/main/java/com/example/superchallenge/MainActivity.java
+++ app/src/main/java/com/example/superchallenge/MainActivity.java
@@ -28,6 +28,9 @@ import android.view.Menu;
 public class MainActivity extends AppCompatActivity
     implements NavigationView.OnNavigationItemSelectedListener{
 
+
+
+
     private AppBarConfiguration mAppBarConfiguration;
 
     @Override
