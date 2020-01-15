efc4ac26b3225b025982653533a1219742499b01
Su Min <52053669+Jo-Sumin@users.noreply.github.com>
2020-01-15 20:40:30 +0900

    2020.01.15. pm 8:39 content_donation_item

--- app/src/main/java/com/example/superchallenge/ContentDonationItemActivity.java
+++ app/src/main/java/com/example/superchallenge/ContentDonationItemActivity.java
@@ -18,13 +18,13 @@ public class ContentDonationItemActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_donation_item);
 
+        image = findViewById(R.id.image_data);
         text1 = findViewById(R.id.title_data);
         text2 = findViewById(R.id.content_data);
-        image = findViewById(R.id.image_data);
 
         Intent intent = getIntent();
         text1.setText(intent.getStringExtra("text1"));
-        text2.setText(intent.getStringExtra("content"));
+        text2.setText(intent.getStringExtra("text2"));
         image.setImageResource(intent.getIntExtra("image", 0));
     }
 }
