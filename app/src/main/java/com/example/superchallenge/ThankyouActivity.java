ab530478ae8a7af1d4dc243f20a30961b8921e66
Eunno An <12171647@inha.edu>
2020-01-16 23:16:32 +0900

    Merge branch 'master' of https://github.com/Eunno-An/SuperChallenge
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    
    thankyouActivity animation

--- app/src/main/java/com/example/superchallenge/ThankyouActivity.java
+++ app/src/main/java/com/example/superchallenge/ThankyouActivity.java
@@ -1,14 +1,28 @@
 package com.example.superchallenge;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class ThankyouActivity extends AppCompatActivity {
-
+    private String strNickName;
+    private String strProfile;
+    private String struserID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thankyou);
+
+        Intent intent = getIntent();
+        strNickName = intent.getStringExtra("name");
+        strProfile = intent.getStringExtra("profile");
+        struserID = intent.getStringExtra("id"); // long은 id
+    }
+    @Override
+    public void onBackPressed() {
+
     }
 }
